4d100eb449e2743a30b5cba3da887ddf28c47b4a
maherhosn <38202267+maherhosn@users.noreply.github.com>
2024-11-25 02:51:17 -0500

    fixing the directories to work on Render

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,10 +42,10 @@ console.log("This is the server configuration!!!");
   ));
 
   // if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   // }
   
